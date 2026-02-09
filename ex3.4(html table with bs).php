4344e4252c9260a562ecaa8d8f070690ef84896b
Anup1011581 <amk1010362@student.hamk.fi>
2026-02-09 19:49:48 +0200

    Add files via upload

---
+++ ex3.4(html table with bs).php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>HTML Table with Bootstrap</h2>
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Anup</td>
+        <td>Kandel</td>
+        <td>anupkandel123@gmail.com</td>
+      </tr>      
+      <tr class="success">
+        <td>Kashmir </td>
+        <td>Aryal</td>
+        <td>KA12@gmail.com</td>
+      </tr>
+      <tr class="danger">
+        <td>acid</td>
+        <td>burn</td>
+        <td>acid1@gmail.com</td>
+      </tr>
+      
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
+
